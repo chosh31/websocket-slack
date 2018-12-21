cbbd2e15bfdd50040ac3d288e50c2e673b819386
shcho <sunghwancho2011@gmail.com>
2018-12-21 18:38:41 +0900

    Update: apply client (browser debug)

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const websocket = require('./websocket');
-websocket.proccessConnect();
+const server = require('./server');
+const path = require('path');
+const minimist = require('minimist');
+
+let args = minimist(process.argv.slice(2), {  
+    default: {
+        mode: 'all', //['all', 'websocket', 'server']
+        port: 3000
+    },
+});
+
+if (args.mode !== 'server') {
+    websocket.proccessConnect();
+}
+
+if (args.mode !== 'websocket') {
+    server.get('/', function(req, res) {
+      res.sendFile(path.join(__dirname+'/client/index.html'));
+    });
+
+    server.listen(3000, function(){
+        console.log('Express server has started on port 3000')
+    });
+}
